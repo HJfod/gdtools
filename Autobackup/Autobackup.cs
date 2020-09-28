299971fc13f41583ba44d10249aee785bd68ff8a
hjfod <dreadrollmusic@gmail.com>
2020-09-28 08:33:42 +0300

    a

---
+++ Autobackup/Autobackup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Windows.UI.Notifications;
+
+namespace autobackup {
+    class Autobackup {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
